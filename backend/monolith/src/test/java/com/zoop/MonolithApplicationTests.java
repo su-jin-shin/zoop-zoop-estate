8ab04920fede2a3f8d13d4d02613dfd8b78198b4
su-jin-shin <sujinshin1@gmail.com>
2025-05-24 14:57:02 +0900

    chore: initialize Spring Boot monolith project

---
+++ backend/monolith/src/test/java/com/zoop/MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
