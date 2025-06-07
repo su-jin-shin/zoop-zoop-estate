ddf6a37d86721091da58db2ccbffec2861e0b1d7
su-jin-shin <sujinshin1@gmail.com>
2025-06-07 17:25:12 +0900

    perf: 매물 1페이지 비동기 병렬 처리 적용으로 크롤링 속도 개선

--- ai/estate_crawling/crawler.py
+++ ai/estate_crawling/crawler.py
@@ -36,6 +36,74 @@ async def fetch_json(session: ClientSession, url: str) -> dict:
         return {}
 
 
+async def handle_article(article, session, dong_code, real_estate_type_code, v_complex_cache, a_complex_cache):
+    article_no = article.get('articleNo')
+    detail_url = f'https://new.land.naver.com/api/articles/{article_no}?complexNo='
+    detail_data = await fetch_json(session, detail_url)
+
+    # 단지 정보 불러오기(빌라, 원룸-투룸)
+    if real_estate_type_code in ['VL:YR', 'DDDGG:DSD']:
+
+        if article.get('isComplex'):  # 해당 매물의 단지가 존재하면 단지 정보를 가져온다.
+
+            # 해당 매물 정보
+            property_info_url = f'https://new.land.naver.com/api/property/article/rent/{article["articleRealEstateTypeCode"]}/{article_no}/{article["tradeTypeCode"]}'
+            property_info_data = await fetch_json(session, property_info_url)
+            # 해당 매물 정보에서 단지 정보 추출
+            complex_info = property_info_data.get('data', {}).get('communalComplexInfo', {})  # 해당 key가 없으면 {}를 반환
+            complex_no = complex_info.get('complexNumber')  # 해당 key가 없으면 None을 반환
+
+            if complex_no:  # complex_no가 None이나 빈 문자열이 아니면
+                article['complexInfo'] = complex_info
+
+                if complex_no not in v_complex_cache:  # 캐시에 저장되어 있지 않다면
+                    complex_info_url = f'https://new.land.naver.com/api/property/complex/villa/{complex_no}'
+                    complex_info_data = await fetch_json(session, complex_info_url)
+
+                    complex_photo_url = f'https://new.land.naver.com/api/property/complex/villa/{complex_no}/photo'
+                    complex_photo_data = await fetch_json(session, complex_photo_url)
+
+                    # 캐시에 저장
+                    v_complex_cache[complex_no] = {
+                        'complexDetails': complex_info_data['data'],
+                        'photoInfo': complex_photo_data['data'],
+                    }
+
+                # 캐시에서 가져와서 단지 정보 삽입
+                article['complexInfo'].update(v_complex_cache[complex_no])  # 내용 추가
+
+    # 단지 정보 불러오기(아파트, 오피스텔)
+    elif real_estate_type_code in ['APT', 'OPST']:
+        complex_no = detail_data.get('articleDetail', {}).get('hscpNo')
+
+        if complex_no:
+            if complex_no not in a_complex_cache:
+
+                complex_info_url = f'https://new.land.naver.com/api/regions/complexes?cortarNo={dong_code}&realEstateType={real_estate_type_code}&order=rank'
+                complex_info_data = await fetch_json(session, complex_info_url)
+
+                for complex in complex_info_data.get('complexList', []):
+                    if complex['complexNo'] == complex_no:
+                        matched_complex = complex
+
+                        # 좀 더 상세한 단지 정보 (일단 여기서는 사진만 가져옴)
+                        complex_detail_info_url = f'https://new.land.naver.com/api/complexes/{complex_no}'
+                        complex_detail_info_data = await fetch_json(session, complex_detail_info_url)
+
+                        a_complex_cache[complex_no] = {
+                            'basicInfo': matched_complex,
+                            'photos': complex_detail_info_data.get('photos', [])
+                        }
+                        break
+
+            # 캐시에서 가져와서 단지 정보 삽입
+            article['complexInfo'] = a_complex_cache[complex_no]['basicInfo']
+            article['complexInfo']['photos'] = a_complex_cache[complex_no]['photos']
+
+    article['articleDetails'] = detail_data
+    return article
+
+
 async def fetch_articles_by_dong(session: ClientSession, cond: dict):
     dong_code = cond['dong']['code']
     dong_name = cond['dong']['name']
@@ -76,79 +144,14 @@ async def fetch_articles_by_dong(session: ClientSession, cond: dict):
         if not articles:
             break
 
-        print(f'********** [{dong_name} / p{page}] 매물 {len(data["articleList"])}건 수집됨 **********')
-
-        print(f'********** 매물 개수: {len(articles)} **********')
+        print(f'********** 매물 개수: [{dong_name} / p{page}] 매물 {len(articles)}건 수집됨 **********')
         total_articles += len(articles)
         print(f'********** 총 매물 개수: {total_articles} **********')
 
-        for article in articles:
-            article_no = article.get('articleNo')
-            detail_url = f'https://new.land.naver.com/api/articles/{article_no}?complexNo='
-            detail_data = await fetch_json(session, detail_url)  # 빌라 단지 갖고오는건 detail_data가 {}를 리턴해도 그다지 상관없음 근데 아파트단지는 영향이 있음.
-
-            # 단지 정보 불러오기(빌라, 원룸-투룸)
-            if real_estate_type_code in ['VL:YR', 'DDDGG:DSD']:
-
-                if article.get('isComplex'):  # 해당 매물의 단지가 존재하면 단지 정보를 가져온다.
-
-                    # 해당 매물 정보
-                    property_info_url = f'https://new.land.naver.com/api/property/article/rent/{article["articleRealEstateTypeCode"]}/{article_no}/{article["tradeTypeCode"]}'
-                    property_info_data = await fetch_json(session, property_info_url)
-                    # 해당 매물 정보에서 단지 정보 추출
-                    complex_info = property_info_data.get('data', {}).get('communalComplexInfo', {})  # 해당 key가 없으면 {}를 반환
-                    complex_no = complex_info.get('complexNumber')  # 해당 key가 없으면 None을 반환
-
-                    if complex_no: # complex_no가 None이나 빈 문자열이 아니면
-                        article['complexInfo'] = complex_info
-
-                        if complex_no not in v_complex_cache:  # 캐시에 저장되어 있지 않다면
-                            complex_info_url = f'https://new.land.naver.com/api/property/complex/villa/{complex_no}'
-                            complex_info_data = await fetch_json(session, complex_info_url)
-
-                            complex_photo_url = f'https://new.land.naver.com/api/property/complex/villa/{complex_no}/photo'
-                            complex_photo_data = await fetch_json(session, complex_photo_url)
-
-                            # 캐시에 저장
-                            v_complex_cache[complex_no] = {
-                                'complexDetails': complex_info_data['data'],
-                                'photoInfo': complex_photo_data['data'],
-                            }
-
-                        # 캐시에서 가져와서 단지 정보 삽입
-                        article['complexInfo'].update(v_complex_cache[complex_no]) # 내용 추가
-
-
-            # 단지 정보 불러오기(아파트, 오피스텔)
-            if real_estate_type_code in ['APT', 'OPST']:
-                complex_no = detail_data.get('articleDetail', {}).get('hscpNo')
-
-                if complex_no:
-                    if complex_no not in a_complex_cache:
-
-                        complex_info_url = f'https://new.land.naver.com/api/regions/complexes?cortarNo={dong_code}&realEstateType={real_estate_type_code}&order=rank'
-                        complex_info_data = await fetch_json(session, complex_info_url)
-
-                        for complex in complex_info_data.get('complexList', []):
-                            if complex['complexNo'] == complex_no:
-                                matched_complex = complex
-
-                                # 좀 더 상세한 단지 정보 (일단 여기서는 사진만 가져옴)
-                                complex_detail_info_url = f'https://new.land.naver.com/api/complexes/{complex_no}'
-                                complex_detail_info_data = await fetch_json(session, complex_detail_info_url)
-
-                                a_complex_cache[complex_no] = {
-                                    'basicInfo': matched_complex,
-                                    'photos': complex_detail_info_data.get('photos', [])
-                                }
-                                break
-                    
-                    # 캐시에서 가져와서 단지 정보 삽입
-                    article['complexInfo'] = a_complex_cache[complex_no]['basicInfo']
-                    article['complexInfo']['photos'] = a_complex_cache[complex_no]['photos']
-
-            article['articleDetails'] = detail_data
-            all_details.append(article)
+        tasks = [
+            handle_article(article, session, dong_code, real_estate_type_code, v_complex_cache, a_complex_cache) for article in articles
+        ]
+        all_details = await asyncio.gather(*tasks)
 
     # 마지막에 한 번에 저장   
     os.makedirs('output', exist_ok=True)
